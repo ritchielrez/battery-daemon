aaab21372456a9c95cd46993d4f314d5065ef59b
RitchielRez <ritchielrez@gmail.com>
2023-12-21 22:56:14 +0600

    feat: send notifications if the percentage is low

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 )
 
-const warning_level = 20
-const fatal_level = 10
+const warning_level = "20%"
+const fatal_level = "10%"
 
 type charging_status int
 
@@ -45,6 +45,7 @@ func sendNotification(msg string) {
 
 func compareBatteryStatus(previous, current charging_status) {
 	if previous == 0 && current == 0 {
+		log.Fatalf("BatteryStates were not initialized\n")
 		return
 	}
 	if previous != current {
@@ -56,6 +57,19 @@ func compareBatteryStatus(previous, current charging_status) {
 		case charged:
 			sendNotification("Battery is now fully charged")
 		}
+	}
+}
+
+func checkBatteryPercentage(percentage string) {
+	if percentage == "" {
+		log.Fatalf("BatteryState was not properly initialized\n")
+		return
+	}
+	if percentage <= warning_level {
+		sendNotification("Battery is low")
+	}
+	if percentage <= fatal_level {
+		sendNotification("Battery is really low")
 	}
 }
 
@@ -99,72 +113,73 @@ func main() {
 
 	batteries_list := make(map[string]*BatteryState)
 
-	// for {
-	power_devices := getPowerDevicesList()
+	for {
+		power_devices := getPowerDevicesList()
 
-	for _, power_device := range power_devices {
-		if matchString("battery", power_device) {
-			battery_info_list := getBatteryInfoList(power_device)
+		for _, power_device := range power_devices {
+			if matchString("battery", power_device) {
+				battery_info_list := getBatteryInfoList(power_device)
 
-			battery_state, exists := batteries_list[power_device]
-			if !exists {
-				battery_state = &BatteryState{}
-				batteries_list[power_device] = battery_state
+				battery_state, exists := batteries_list[power_device]
+				if !exists {
+					battery_state = &BatteryState{}
+					batteries_list[power_device] = battery_state
+
+					battery_state.current = &Battery{
+						model_name: "",
+						percentage: "",
+						status:     charging_status(0),
+					}
+				}
+
+				log.Println("Before creating battery_state")
+				battery_state.previous = battery_state.current
+				log.Println("After creating battery_state")
 
 				battery_state.current = &Battery{
 					model_name: "",
 					percentage: "",
 					status:     charging_status(0),
 				}
-			}
 
-			log.Println("Before creating battery_state")
-			battery_state.previous = battery_state.current
-			log.Println("After creating battery_state")
-
-			battery_state.current = &Battery{
-				model_name: "",
-				percentage: "",
-				status:     charging_status(0),
-			}
-
-			for _, battery_info := range battery_info_list {
-				if matchString("model", battery_info) {
-					battery_state.current.model_name = strings.TrimSpace(
-						strings.Split(battery_info, ":")[1],
-					)
-					log.Printf("Battery model: %s\n", battery_state.current.model_name)
-					continue
-				}
-				if matchString("percentage", battery_info) {
-					battery_state.current.percentage = strings.TrimSpace(
-						strings.Split(battery_info, ":")[1],
-					)
-					log.Printf(
-						"Battery percentage: %s\n",
-						battery_state.current.percentage,
-					)
-					continue
-				}
-				if matchString("state", battery_info) {
-					battery_charging_state := strings.TrimSpace(
-						strings.Split(strings.TrimSpace(battery_info), ":")[1],
-					)
-					if battery_charging_state == "charging" &&
-						battery_state.current.status != charging {
-						battery_state.current.status = charging
-					} else if battery_charging_state == "discharging" && battery_state.current.status != discharging {
-						battery_state.current.status = discharging
-					} else if battery_charging_state == "fully-charged" && battery_state.current.status != charged {
-						battery_state.current.status = charged
+				for _, battery_info := range battery_info_list {
+					if matchString("model", battery_info) {
+						battery_state.current.model_name = strings.TrimSpace(
+							strings.Split(battery_info, ":")[1],
+						)
+						log.Printf("Battery model: %s\n", battery_state.current.model_name)
+						continue
 					}
-					log.Printf("Battery state: %s\n", battery_charging_state)
+					if matchString("percentage", battery_info) {
+						battery_state.current.percentage = strings.TrimSpace(
+							strings.Split(battery_info, ":")[1],
+						)
+						log.Printf(
+							"Battery percentage: %s\n",
+							battery_state.current.percentage,
+						)
+						continue
+					}
+					if matchString("state", battery_info) {
+						battery_charging_state := strings.TrimSpace(
+							strings.Split(strings.TrimSpace(battery_info), ":")[1],
+						)
+						if battery_charging_state == "charging" &&
+							battery_state.current.status != charging {
+							battery_state.current.status = charging
+						} else if battery_charging_state == "discharging" && battery_state.current.status != discharging {
+							battery_state.current.status = discharging
+						} else if battery_charging_state == "fully-charged" && battery_state.current.status != charged {
+							battery_state.current.status = charged
+						}
+						log.Printf("Battery state: %s\n", battery_charging_state)
+					}
 				}
+
+				compareBatteryStatus(battery_state.previous.status, battery_state.current.status)
+				checkBatteryPercentage(battery_state.current.percentage)
 			}
 
-			compareBatteryStatus(battery_state.previous.status, battery_state.current.status)
 		}
-
-		// }
 	}
 }
